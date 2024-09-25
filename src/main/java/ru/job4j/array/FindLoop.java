1fb63ff902ce3c351623cf762a0cf90318b19ad6
andrewnnov <andrewnnov@yandex.ru>
2024-09-25 20:21:53 +0300

    find element in array with start and finish

---
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+public class FindLoop {
+
+    public static int indexOf(int[] data, int element) {
+        int result = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        return result;
+    }
+
+    public static int indexInRange(int[] data, int element, int start, int finish) {
+
+        int result = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        for (int index = start; index <= finish; index++) {
+            if (data[index] == element) {
+                result = index;
+                break;
+            }
+        }
+        return result;
+    }
+}
