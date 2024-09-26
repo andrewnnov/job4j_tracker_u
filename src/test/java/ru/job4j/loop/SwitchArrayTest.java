7e0360590c807fd781e832411c0795534164dd15
andrewnnov <andrewnnov@yandex.ru>
2024-09-26 18:54:20 +0300

    move to another dir switchAr

--- src/test/java/ru/job4j/loop/SwitchArrayTest.java
+++ src/test/java/ru/job4j/loop/SwitchArrayTest.java
@@ -1,9 +1,9 @@
 package ru.job4j.loop;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.array.SwitchArray;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class SwitchArrayTest {
     @Test
