7bb2eb4bbb140c5cd811ce6729b2d0709e70fc4c
andrewnnov <andrewnnov@yandex.ru>
2024-10-15 23:35:00 +0300

    car service

---
+++ src/main/java/ru/job4j/encapsulation/CarService.java
@@ -0,0 +1,10 @@
+package ru.job4j.encapsulation;
+
+public class CarService {
+    public static void main(String[] args) {
+        Status toyota = Status.FINISHED;
+        Status volvo = Status.WAITING;
+        System.out.println(toyota);
+        System.out.println(volvo);
+    }
+}
